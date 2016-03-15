056440877a100535d589070e8a4fa890e0267f6f
Hemant Jadon <hemantjadon.hemu@gmail.com>
2016-03-15 12:08:32 +0530

    Generic Class Based Views : Short code

--- LearnDjangoRestFramework/snippets/views.py
+++ LearnDjangoRestFramework/snippets/views.py
@@ -17,30 +17,15 @@ from snippets.serializers import SnippetSerializer
 
 # Create your views here.
 
-class SnippetList(mixins.ListModelMixin,mixins.CreateModelMixin,generics.GenericAPIView):
+class SnippetList(generics.ListCreateAPIView):
 	permission_classes = [IsAdminUser]
 	queryset = Snippet.objects.all()
 	serializer_class = SnippetSerializer
-	
-	def get(self,request,*args,**kwargs):
-		return self.list(request,*args,**kwargs)
-		
-	def post(self,request,format=None):
-		return self.create(request,*args,**kwargs)
 
-class SnippetDetail(mixins.RetrieveModelMixin,mixins.UpdateModelMixin,mixins.DestroyModelMixin,generics.GenericAPIView):
+class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
 	permission_classes = [IsAdminUser]
 	queryset = Snippet.objects.all()
 	serializer_class = SnippetSerializer
-		
-	def get(self,request,*args,**kwargs):
-		return self.retrieve(request,*args,**kwargs)
-	
-	def put(self,request,*args,**kwargs):
-		return self.update(request,*args,**kwargs)
-	
-	def delete(self,request,pk,format=None):
-		return self.destroy(request,*args,**kwargs)
 		
 class SnippetViewSet(viewsets.ModelViewSet):
 	permission_classes = [IsAdminUser]

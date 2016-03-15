843394e528d323110506cc2df127dbf2c3e0a8f3
Hemant Jadon <hemantjadon.hemu@gmail.com>
2016-03-15 11:48:08 +0530

    class based view basic APIView

--- LearnDjangoRestFramework/snippets/views.py
+++ LearnDjangoRestFramework/snippets/views.py
@@ -6,50 +6,57 @@ from rest_framework.renderers import JSONRenderer
 from rest_framework.parsers import JSONParser
 from rest_framework import viewsets
 from rest_framework import status
-from rest_framework.decorators import api_view,permission_classes
+from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAdminUser
+
 
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
 
 # Create your views here.
 
-@api_view(['GET', 'POST'])
-@permission_classes([IsAdminUser])
-def snippet_list(request):
-    if request.method == 'GET':
-        snippets = Snippet.objects.all()
-        serializer = SnippetSerializer(snippets, many=True,context={'request': request})
-        return Response(serializer.data,status = status.HTTP_200_OK)
-
-    elif request.method == 'POST':
-        serializer = SnippetSerializer(data=request.data,context={'request': request})
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-@api_view(['GET','PUT','DELETE'])
-@permission_classes([IsAdminUser])
-def snippet_detail(request,pk):
-	try:
-		snippet = Snippet.objects.get(pk=pk)
-	except Snippet.DoesNotExist:
-		return Response(status=status.HTTP_404_NOT_FOUND)
+class SnippetList(APIView):
+	permission_classes = [IsAdminUser]
+	queryset = Snippet.objects.all()
+	
+	def get(self,request,format=None):
+		snippets = Snippet.objects.all()
+		serializer = SnippetSerializer(snippets, many=True,context={'request': request})
+		return Response(serializer.data,status = status.HTTP_200_OK)
 		
-	if request.method == 'GET':
+	def post(self,request,format=None):
+		serializer = SnippetSerializer(data=request.data,context={'request': request})
+		if serializer.is_valid():
+			serializer.save()
+			return Response(serializer.data, status=status.HTTP_201_CREATED)
+			return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class SnippetDetail(APIView):
+	permission_classes = [IsAdminUser]
+	queryset = Snippet.objects.all()
+	
+	def get_object(self,pk):
+		try:
+			return Snippet.objects.get(pk=pk)
+		except Snippet.DoesNotExist:
+			return Response(status=status.HTTP_404_NOT_FOUND)
+		
+	def get(self,request,pk,format=None):
+		snippet = self.get_object(pk)
 		serializer = SnippetSerializer(snippet,context={'request': request})
 		return Response(serializer.data,status=status.HTTP_200_OK)
 	
-	elif request.method == 'PUT':
+	def put(self,request,pk,format=None):
+		snippet = self.get_object(pk)
 		serializer = SnippetSerializer(snippet,data=request.data,context={'request': request})
 		if serializer.is_valid():
 			return Response(serializer.data,status=status.HTTP_200_OK)
 		else:
 			return Response(serializer.data,status=status.HTTP_400_BAD_REQUEST)
 	
-	elif request.method == 'DELETE':
+	def delete(self,request,pk,format=None):
+		snippet = self.get_object(pk)
 		snippet.delete()
 		return Response(status=status.HTTP_204_NO_CONTENT)
 		
